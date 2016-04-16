dcafd1cf536d08078019674e0dd910e026f0015d
Juho Saarelainen <bradurdung@gmail.com>
2016-04-16 16:49:18 +0300

    :house: Landing spot and detection of player reaching it.

---
+++ The Magnificent Lepus/Assets/Scripts/Level/End.cs
@@ -0,0 +1,17 @@
+// Date   : 16.04.2016 16:46
+// Project: Game
+// Author : bradur
+
+using UnityEngine;
+using System.Collections;
+
+public class End : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (collision2D.gameObject.tag == "Player")
+        {
+            Debug.Log("Player hit me!");
+        }
+    }
+}
